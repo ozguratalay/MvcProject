73114930af860a187c2c9e3b23d625d83ac134fa
Özgür Atalay <ozratalay@outlook.com>
2022-12-08 14:46:44 +0300

    Message entity added, Inbox view added

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator: AbstractValidator<Message>
+    {
+        public MessageValidator() {
+            RuleFor(x => x.SenderMail).NotEmpty().WithMessage("Boş bırakılamaz!");
+            RuleFor(x => x.SenderMail).MaximumLength(50).WithMessage("50 karakterden fazla giriş yapılamaz!");
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Boş bırakılamaz!");
+            RuleFor(x => x.ReceiverMail).MaximumLength(50).WithMessage("50 karakterden fazla giriş yapılamaz!");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Boş bırakılamaz!");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("100 karakterden fazla giriş yapılamaz!");
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
