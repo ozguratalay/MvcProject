68140ea1725446c785dcd78c3289c481f80af8e8
Özgür Atalay <ozratalay@outlook.com>
2022-12-25 11:22:26 +0300

    Gallery added

---
+++ DataAccessLayer/Migrations/202212250720120_add_ImageFile.cs
@@ -0,0 +1,27 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_ImageFile : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ImageFiles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 100),
+                        Path = c.String(maxLength: 250),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ImageFiles");
+        }
+    }
+}
