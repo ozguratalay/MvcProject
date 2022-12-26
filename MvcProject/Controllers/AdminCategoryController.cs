1fe844c331656e01c4210a11e3a154f1d436339e
Özgür Atalay <ozratalay@outlook.com>
2022-12-27 01:25:31 +0300

    Admin login page added. Category page authorized Session added

--- MvcProject/Controllers/AdminCategoryController.cs
+++ MvcProject/Controllers/AdminCategoryController.cs
@@ -14,6 +14,7 @@ namespace MvcProject.Controllers
     public class AdminCategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        [Authorize]
         public ActionResult Index()
         {
             var categoryValues = cm.GetList();
